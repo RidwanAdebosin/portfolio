13058556faae1d6ea9b420409e760eaf55b67b38
Apple <apple@Apples-MacBook-Pro.local>
2024-06-25 11:14:39 +0100

    custom class added

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { links } from '../assets/data';
 const Navbar = () => {
   return (
     <nav className='bg-emerald-100 '>
-      <div className='mx-auto max-w-7xl  px-8 py-4 flex flex-col  sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+      <div className='align-element py-4 flex flex-col  sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
         <h2 className='text-3xl font-bold'>
           Web
           <span className='text-emerald-600'>Dev</span>
